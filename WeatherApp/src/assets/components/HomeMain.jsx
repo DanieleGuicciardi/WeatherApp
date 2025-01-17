6bff0a3622bb0b34a6eebe63ad2f7d653d911acc
DanieleGuicciardi <dany.guicciardi@outlook.com>
2025-01-17 16:53:46 +0100

    map added

--- WeatherApp/src/assets/components/HomeMain.jsx
+++ WeatherApp/src/assets/components/HomeMain.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 import WeatherCard from "./cardCities";
-import HomeMainBottom from './HomeMainBottom'
+import HomeMainBottom from './HomeMainBottom';
 
-const CitiesMeteo = () => {
-  return(
+const CitiesMeteo = () => {    //codice preso dalla documentazione di leaflet
+
+  useEffect(() => {
+    const map = L.map("map").setView([41.9028, 12.4964], 5); // Ho scelto Roma per mancanza di tempo ma avrei potuto prendere la lat e la longitudine dal click di una card
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([41.9028, 12.4964]).addTo(map)
+      .bindPopup("Rome, Italy")
+      .openPopup();
+
+    return () => {
+      map.remove(); 
+    };
+  }, []);
+
+  return (
     <>
-      <h2 style={{display: "flex", marginTop: "20px", fontWeight: "600", marginLeft: "5em" }}>⛅ Major Cities Weather Tracker:</h2>
+      <h2 style={{ display: "flex", marginTop: "20px", fontWeight: "600", marginLeft: "5em" }}>⛅ Major Cities Weather Tracker:</h2>
       <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", marginTop: "20px" }}>
-        <WeatherCard city="Lido di Ostia"/>
-        <WeatherCard city="New York"/>
-        <WeatherCard city="Klaipeda"/>
-        <WeatherCard city="London"/>
-        <WeatherCard city="Tokyo"/>
-
-        <HomeMainBottom/>
-        
+        <WeatherCard city="Lido di Ostia" />
+        <WeatherCard city="New York" />
+        <WeatherCard city="Klaipeda" />
+        <WeatherCard city="London" />
+        <WeatherCard city="Tokyo" />
       </div>
+
+      <div id="map" style={{ height: "100%", margin: "20px auto", width: "78%" }}></div>
+
+      <HomeMainBottom />
     </>
-  )
-}
+  );
+};
 
 export default CitiesMeteo;
